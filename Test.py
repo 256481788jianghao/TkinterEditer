d4245517fca2774cf11587694c8f96c77e640f0b
jianghao <2564811788@163.com>
2019-12-11 09:35:18 +0800

    init

---
+++ Test.py
@@ -0,0 +1,14 @@
+'''
+Created on 2019年12月11日
+
+@author: Administrator
+'''
+
+import tkinter as tk
+
+root = tk.Tk()
+
+label = tk.Label(root, text="Hello World", padx=10, pady=10)
+label.pack()
+
+root.mainloop()
